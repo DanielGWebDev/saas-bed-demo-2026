367b3cd8c6957f83aedd7b384c379456f5869e58
Daniel Gillespie <danielgillespie717@gmail.com>
2026-02-12 12:34:03 +0800

    Initial Laravel commit

---
+++ app/Http/Controllers/Api/V1/ContactController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreContactRequest;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+        return response()->json($contacts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreContactRequest $request)
+    {
+        $validated = $request->validated();
+        $contact = Contact::create($validated);
+        return response()->json($contact, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+
+        return response()->noContent();  // 204 No Content
+    }
+}
